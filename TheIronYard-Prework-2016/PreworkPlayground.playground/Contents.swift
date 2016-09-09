2262e9b2594178983abb4ad6081d64a1363ae4e8
rem{e}koh <git@remekoh.me>
2016-09-08 19:55:45 -0500

    initial commit

---
+++ TheIronYard-Prework-2016/PreworkPlayground.playground/Contents.swift
@@ -0,0 +1,77 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// declare a constant named city and assign initial value to be "Houston"
+
+let city = "Houston"
+
+// declare a constant named state and assign initial value to be "Texas"
+
+let state = "Texas"
+
+// declare a var named zipcode and assign initial value to be 77042
+
+var zipcode = 77042
+
+// declare a constant called firstName and assign its initial value "John"
+
+let firstName = "John"
+
+// declare a constant called lastName and assign its initial value "Doe"
+
+let lastName = "Doe"
+
+// declare a constant called fullName which is the concatentation of firstName and lastName. Example "John Doe" when appending firstName and lastName together
+let fullName = firstName + " " + lastName
+
+let cityTemp = 90
+
+// use a string literal to print the values of city and temperature
+// Example: The temperature in Houston is 85
+// Use the city and temperature variables declared above in your string literals
+print("The temperature in \(city) is \(cityTemp)")
+
+// create an array called cities and populate with the following "Houston", "Austin", "San Antonio", "Dallas"
+var cities = ["Houston", "Austin", "San Antonio", "Dallas"]
+
+// Change the "San Antonio" value to be "Sugarland"
+cities[2] = "Sugarland"
+
+cities
+
+// create a String array called names of populate with the following names
+// "John", "Mary", "Alex","Jack"
+// make sure that the names array can only hold String values
+// The following should not work
+// let names = ["John","Mary","Alex",34]
+
+// iterate through the names array and print out all the names
+
+// iterate through the cities array and print each city name after appending the state name. Example "Houston, TX"
+
+// create a dictionary and initialize it with the following:
+// Dictionary[String:String]
+
+//  key = Houston   value = IAH
+// key = Costa Rica   value = San Jose Airport
+// key = Las Angelos   value = LAX Airport
+
+// print the value of the key "Houston"
+
+// iterate through the dictionary and print all the values of the airports
+
+// create a function called greet which prints "greetings to you" message
+
+// call the function greet
+
+// create a function called add which take two arguments of type int and returns the sum of ints
+
+// call the function add and pass the following arguments (2,5)
+
+// create a class called Person with the following properties
+// firstName :String
+// lastName :String
+
+
+// create the object of the class Person and assign the properties firstName and lastName with "John" and "Doe"
